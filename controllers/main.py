1eee246acd04f1ef8181295102ff72ab939b87a4
sirW1SE <odoo@LAPTOP-2G21BLAR.localdomain>
2022-11-08 14:14:32 +0800

    Second Commit

--- controllers/main.py
+++ controllers/main.py
@@ -4,29 +4,28 @@ from odoo import http
 from odoo.http import content_disposition, request
 import io
 import xlsxwriter
-    
 
 
 class SaleExcelReportController(http.Controller):
     @http.route([
         '/sale/excel_report/<model("ng.sale.wizard"):wizard>',
     ], type='http', auth="user", csrf=False)
-    def get_sale_excel_report(self,wizard=None,**args):
-        # the wizard parameter is the primary key that odoo sent 
+    def get_sale_excel_report(self, wizard=None, **args):
+        # the wizard parameter is the primary key that odoo sent
         # with the get_excel_report method in the ng.sale.wizard model
         # contains salesperson, start date, and end date
 
         # create a response with a header in the form of an excel file
         # so the browser will immediately download it
         # The Content-Disposition header is the file name fill as needed
-        
+
         response = request.make_response(
-                    None,
-                    headers=[
-                        ('Content-Type', 'application/vnd.ms-excel'),
-                        ('Content-Disposition', content_disposition('Sales Report in Excel Format' + '.xlsx'))
-                    ]
-                )
+            None,
+            headers=[
+                ('Content-Type', 'application/vnd.ms-excel'),
+                ('Content-Disposition', content_disposition('Sales Report in Excel Format' + '.xlsx'))
+            ]
+        )
 
         # create workbook object from xlsxwriter library
         output = io.BytesIO()
@@ -34,41 +33,50 @@ class SaleExcelReportController(http.Controller):
 
         # create some style to set up the font type, the font size, the border, and the aligment
         title_style = workbook.add_format({'font_name': 'Times', 'font_size': 14, 'bold': True, 'align': 'center'})
-        header_style = workbook.add_format({'font_name': 'Times', 'bold': True, 'left': 1, 'bottom':1, 'right':1, 'top':1, 'align': 'center'})
-        text_style = workbook.add_format({'font_name': 'Times', 'left': 1, 'bottom':1, 'right':1, 'top':1, 'align': 'left'})
-        number_style = workbook.add_format({'font_name': 'Times', 'left': 1, 'bottom':1, 'right':1, 'top':1, 'align': 'right'})
+        header_style = workbook.add_format(
+            {'font_name': 'Times', 'bold': True, 'left': 1, 'bottom': 1, 'right': 1, 'top': 1, 'align': 'center'})
+        text_style = workbook.add_format(
+            {'font_name': 'Times', 'left': 1, 'bottom': 1, 'right': 1, 'top': 1, 'align': 'left'})
+        number_style = workbook.add_format(
+            {'font_name': 'Times', 'left': 1, 'bottom': 1, 'right': 1, 'top': 1, 'align': 'right'})
 
         # loop all selected user/salesperson
         for user in wizard.user_id:
-            # create worksheet/tab per salesperson 
+            # create worksheet/tab per salesperson
             sheet = workbook.add_worksheet(user.name)
             # set the orientation to landscape
             sheet.set_landscape()
             # set up the paper size, 9 means A4
             sheet.set_paper(9)
             # set up the margin in inch
-            sheet.set_margins(0.5,0.5,0.5,0.5)
+            sheet.set_margins(0.5, 0.5, 0.5, 0.5)
 
             # set up the column width
-            sheet.set_column('A:A', 5)
-            sheet.set_column('B:E', 15)
+            sheet.set_column('A:A', 8)
+            sheet.set_column('B:H', 15)
 
             # the report title
             # merge the A1 to E1 cell and apply the style font size : 14, font weight : bold
-            sheet.merge_range('A1:E1', 'Sales Report in Excel Format', title_style)
-            
+            sheet.merge_range('A1:I1', 'Sales Report in Excel Format', title_style)
+
             # table title
             sheet.write(1, 0, 'No.', header_style)
             sheet.write(1, 1, 'Document', header_style)
             sheet.write(1, 2, 'Date', header_style)
             sheet.write(1, 3, 'Customer', header_style)
             sheet.write(1, 4, 'Total', header_style)
+            sheet.write(1, 5, 'Product', header_style)
+            sheet.write(1, 6, 'Internal Reference', header_style)
+            sheet.write(1, 7, 'Quantity', header_style)
+            sheet.write(1, 8, 'Price', header_style)
 
             row = 2
             number = 1
 
-            # search the sales order  
-            orders = request.env['sale.order'].search([('user_id','=',user.id), ('date_order','>=', wizard.start_date), ('date_order','<=', wizard.end_date)])
+            # search the sales order
+            orders = request.env['sale.order'].search(
+                [('user_id', '=', user.id), ('date_order', '>=', wizard.start_date),
+                 ('date_order', '<=', wizard.end_date)])
             for order in orders:
                 # the report content
                 sheet.write(row, 0, number, text_style)
@@ -76,13 +84,20 @@ class SaleExcelReportController(http.Controller):
                 sheet.write(row, 2, str(order.date_order), text_style)
                 sheet.write(row, 3, order.partner_id.name, text_style)
                 sheet.write(row, 4, order.amount_total, number_style)
+                for line in order.order_line:
+                    sheet.write(row, 5, line.product_id.name, text_style)
+                    sheet.write(row, 6, line.product_id.default_code, text_style)
+                    sheet.write(row, 7, line.product_uom_qty, number_style)
+                    sheet.write(row, 8, line.price_unit, number_style)
+                    row += 1
+                    number += 1
 
                 row += 1
                 number += 1
 
             # create a formula to sum the total sales
-            sheet.merge_range('A' + str(row+1) + ':D' + str(row+1), 'Total', text_style)
-            sheet.write_formula(row, 4, '=SUM(E3:E' + str(row) + ')', number_style)
+            sheet.merge_range('A' + str(row + 1) + ':H' + str(row + 1), 'Total', text_style)
+            sheet.write_formula(row, 8, '=SUM(E3:E' + str(row) + ')', number_style)
 
         # return the excel file as a response, so the browser can download it
         workbook.close()
